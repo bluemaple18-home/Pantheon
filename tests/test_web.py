9b78a7d804c2a9c7ba868487a8027ef4f07453d2
bluemaple18-home <bluemaple18@gmail.com>
2026-07-30 08:04:08 +0800

    chore(content): publish multilingual release v0.3.150

--- tests/test_web.py
+++ tests/test_web.py
@@ -39,7 +39,7 @@ from scripts.prerender_article_shells import (
 from scripts.update_articles_hub_dates import render_articles_hub_dates
 
 
-ARTICLE_CACHE_TOKEN = "agy-i18n-0-3-149"
+ARTICLE_CACHE_TOKEN = "agy-i18n-0-3-150"
 
 INITIAL_FIRST_30_ARTICLE_PATHS = [
     "/articles/personality/personality-0001",
