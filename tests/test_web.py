197104c82164a7c571fdef4c89d5b2aa2c53b700
bluemaple18-home <bluemaple18@gmail.com>
2026-09-10 09:37:11 +0800

    chore(content): publish Gemini approved articles v0.3.390

--- tests/test_web.py
+++ tests/test_web.py
@@ -39,7 +39,7 @@ from scripts.prerender_article_shells import (
 from scripts.update_articles_hub_dates import render_articles_hub_dates
 
 
-ARTICLE_CACHE_TOKEN = "agy-i18n-0-3-389"
+ARTICLE_CACHE_TOKEN = "agy-auto-new-v1-20260910-001-01"
 
 INITIAL_FIRST_30_ARTICLE_PATHS = [
     "/articles/personality/personality-0001",
@@ -506,6 +506,7 @@ DAILY_PUBLIC_ARTICLE_PATHS = [
     "/articles/tarot/tarot-1952",
     "/articles/tarot/tarot-1954",
     "/articles/fortune/fortune-0062",
+    "/articles/tarot/tarot-2021",
 ]
 
 CODEX_EMERGENCY_PUBLIC_ARTICLE_PATHS = [
