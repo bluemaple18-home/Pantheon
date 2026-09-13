6ea64d58d08419c27ab95851a37f73d74b43fec3
bluemaple18-home <bluemaple18@gmail.com>
2026-09-14 06:48:10 +0800

    chore(content): publish multilingual release v0.3.468

--- app/web/static/article-admin.js
+++ app/web/static/article-admin.js
@@ -6,7 +6,7 @@ import {
   listArticleRecords,
   listArticleSectionRecords,
   listTagManagementRecords,
-} from "./article-registry.js?v=agy-i18n-0-3-467";
+} from "./article-registry.js?v=agy-i18n-0-3-468";
 
 const dom = {
   boundary: document.querySelector("[data-policy-boundary]"),
--- app/web/static/article-meta.js
+++ app/web/static/article-meta.js
@@ -10,7 +10,7 @@ import {
   listArticleRecords,
   listArticlesForTopic,
   listPublicTagLabelsForArticle,
-} from "./article-registry.js?v=agy-i18n-0-3-467";
+} from "./article-registry.js?v=agy-i18n-0-3-468";
 import {
   ARTICLE_LOCALE_CONFIG,
   ARTICLE_UI_MESSAGES,
