b91fd67a830d345577b10bcadb5063fd8c5fc342
bluemaple18-home <bluemaple18@gmail.com>
2026-09-14 01:03:31 +0800

    chore(content): publish multilingual release v0.3.456

--- app/web/static/app.js
+++ app/web/static/app.js
@@ -1,5 +1,5 @@
 import { buildPredictionPayload, fetchPrediction } from "./api.js";
-import { getArticlePath, getProductThemeRecord, listArticleRecords } from "./article-registry.js?v=agy-i18n-0-3-455";
+import { getArticlePath, getProductThemeRecord, listArticleRecords } from "./article-registry.js?v=agy-i18n-0-3-456";
 import { renderDashboard } from "./dashboard.js";
 import { renderFortunePaper } from "./paper.js";
 
--- app/web/static/article-admin.js
+++ app/web/static/article-admin.js
@@ -6,7 +6,7 @@ import {
   listArticleRecords,
   listArticleSectionRecords,
   listTagManagementRecords,
-} from "./article-registry.js?v=agy-i18n-0-3-455";
+} from "./article-registry.js?v=agy-i18n-0-3-456";
 
 const dom = {
   boundary: document.querySelector("[data-policy-boundary]"),
