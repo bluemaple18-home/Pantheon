9ac1a10c918d1844cf1ac7184793b2a954d9660a
bluemaple18-home <bluemaple18@gmail.com>
2026-09-14 20:22:18 +0800

    chore(content): publish multilingual release v0.3.492

--- app/web/static/app.js
+++ app/web/static/app.js
@@ -1,5 +1,5 @@
 import { buildPredictionPayload, fetchPrediction } from "./api.js";
-import { getArticlePath, getProductThemeRecord, listArticleRecords } from "./article-registry.js?v=agy-auto-new-v1-20260914-091-01";
+import { getArticlePath, getProductThemeRecord, listArticleRecords } from "./article-registry.js?v=agy-i18n-0-3-492";
 import { renderDashboard } from "./dashboard.js";
 import { renderFortunePaper } from "./paper.js";
 
