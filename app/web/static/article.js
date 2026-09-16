ca6b86039acdbae30bdfa967f4a2b7b09f9817c4
bluemaple18-home <bluemaple18@gmail.com>
2026-09-16 10:55:12 +0800

    chore(content): publish multilingual release v0.3.575

--- app/web/static/article.js
+++ app/web/static/article.js
@@ -1,4 +1,4 @@
-import { buildArticleContent } from "./article-meta.js?v=agy-rewrite-20260916-05";
+import { buildArticleContent } from "./article-meta.js?v=agy-i18n-0-3-575";
 import { applyArticleSeo } from "./article-seo.js?v=article-content-20260710-10";
 
 const INLINE_TOPIC_MAX_LINKS = 8;
