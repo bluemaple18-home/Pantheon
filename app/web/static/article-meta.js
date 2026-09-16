20cd10553ae481ca707f083a29bc587664465c15
bluemaple18-home <bluemaple18@gmail.com>
2026-09-16 09:55:14 +0800

    chore(content): publish multilingual release v0.3.572

--- app/web/static/article-meta.js
+++ app/web/static/article-meta.js
@@ -10,7 +10,7 @@ import {
   listArticleRecords,
   listArticlesForTopic,
   listPublicTagLabelsForArticle,
-} from "./article-registry.js?v=agy-rewrite-20260916-04";
+} from "./article-registry.js?v=agy-i18n-0-3-572";
 import {
   ARTICLE_LOCALE_CONFIG,
   ARTICLE_UI_MESSAGES,
