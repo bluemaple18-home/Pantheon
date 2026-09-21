7a97510fa04b2d1a710932fba0441b7cf899655a
bluemaple18-home <bluemaple18@gmail.com>
2026-09-21 23:02:19 +0800

    chore(content): publish Gemini rewrite release v0.3.615

--- app/web/static/app.js
+++ app/web/static/app.js
@@ -1,5 +1,5 @@
 import { buildPredictionPayload, fetchPrediction } from "./api.js";
-import { getArticlePath, getProductThemeRecord, listArticleRecords } from "./article-registry.js?v=agy-auto-new-v1-20260920-037-01";
+import { getArticlePath, getProductThemeRecord, listArticleRecords } from "./article-registry.js?v=agy-rewrite-20260921-04";
 import { renderDashboard } from "./dashboard.js";
 import { renderFortunePaper } from "./paper.js";
 
